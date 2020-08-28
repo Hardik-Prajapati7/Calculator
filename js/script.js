6c986f520d44c36d95f6d673ec24fec3bb5908ff
Hardik-Prajapati <kaliyanihardik007@gmail.com>
2020-08-28 10:34:06 +0530

    minor changes in files
    Now its fine

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ let screen=document.getElementById('screen');
 buttons=document.querySelectorAll('button');
 
 
+
+let history='';
+
 let screenValue='';
 
 for (item of buttons) {
@@ -11,7 +14,6 @@ for (item of buttons) {
         console.log('Button text is ', buttonText);
         if (buttonText == 'X') {
             buttonText = '*';
-            screenValue += buttonText;
             screen.value = screenValue;
         }
         if (buttonText == '÷') {
@@ -38,7 +40,56 @@ for (item of buttons) {
             screenValue += buttonText;
             screen.value = screenValue;
         }
+        history=screenValue;
+
+        console.log('History is',history);
+
+
+       document.getElementById("history").innerHTML = history;
 
     })
+}
+
+function downloadCSV(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV file
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // Create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Hide download link
+    downloadLink.style.display = "none";
+
+    // Add the link to DOM
+    document.body.appendChild(downloadLink);
+
+    // Click download link
+    downloadLink.click();
+}
+
+function exportTableToCSV(filename) {
+    var csv = [];
+    var rows = document.querySelectorAll("table");
+    
+    for (var i = 1; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td");
+        
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        
+        csv.push(row.join(","));        
+    }
+
+    // Download CSV file
+    downloadCSV(csv.join("\n"), filename);
 }
 
